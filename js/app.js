b6e017d2176339eca77468241e1fce315d5e311b
Korèn <kpapazian@edenschool.fr>
2023-01-12 17:04:13 +0100

    carrousel

--- js/app.js
+++ js/app.js
@@ -2,39 +2,25 @@ let arrow_left = document.getElementById("arrow_left");
 let arrow_right = document.getElementById("arrow_right");
 
 let img = document.getElementById("img_middle");
-let img_left = document.getElementById("img_left");
-let img_right = document.getElementById("img_right");
 
-let imgs = ["img/image-slide-1.jpg", "img/image-slide-2.jpg", "img/image-slide-3.jpg", "img/image-slide-4.jpg", "img/image-slide-5.jpg"];
-
-let i = 0;
-
-img_right.src = imgs[4];
-img.src = imgs[i];
-img_left.src = imgs[i+1];
+let i = 1;
 
 arrow_left.addEventListener("click", () => {
-    i ++;
-    if (i == imgs.length) {
-        i = 0;
-        img.src = imgs[i];
+    i++;
+    if (i > 5) {
+        i = 1;
+        img.src = `img/image-slide-${i}.jpg`;
+    } else {
+        img.src = `img/image-slide-${i}.jpg`;
     }
-
-    console.log(i);
-    img.src = imgs[i];
-    img_left.src = imgs[i+1];
-    img_right.src = imgs[i-1];
 });
 
 arrow_right.addEventListener("click", () => {
-    i --;
-    if (i < 0) {
-        i = 4;
-        img.src = imgs[i];
+    i--;
+    if (i == 0) {
+        i = 5;
+        img.src = `img/image-slide-${i}.jpg`;
+    } else {
+        img.src = `img/image-slide-${i}.jpg`;
     }
-
-    console.log(i);
-    img.src = imgs[i];
-    img_left.src = imgs[i+1];
-    img_right.src = imgs[i-1];
 });
